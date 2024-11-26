97556932db283b37b4bdadcbdf45f9bce036c5c4
buptsdz <508936331@qq.com>
2024-11-26 10:07:20 +0800

    主页添加个人网站链接

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -6,6 +6,11 @@
       <el-button type="primary" @click="$router.push('/color-game')">颜色游戏</el-button>
       <!-- 后续可以在这里添加更多工具的入口 -->
     </div>
+    <div class="tool-list">
+      <el-button type="primary">
+        <a href="http://sue.sparkflare.cn" target="_blank" style="color: inherit; text-decoration: none;">我的个人网站</a>
+      </el-button>
+    </div>
   </div>
 </template>
 
