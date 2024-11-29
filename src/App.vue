d0722317fa3d1d3cabe4c065c4b755f725fb46b0
buptsdz <508936331@qq.com>
2024-11-29 15:19:52 +0800

    有个代码忘记保存

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <div class="background-random-profile"></div>
     <el-container>
       <el-header>
         <el-menu :router="true" mode="horizontal" :default-active="$route.path">
